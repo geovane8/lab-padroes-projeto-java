6127c2d5ba8795b755f978f9471f585c9010d2b2
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-04-29 00:22:50 -0300

    Update SingletonLazy.java

--- src/one/digitalinnovation/gof/singleton/SingletonLazy.java
+++ src/one/digitalinnovation/gof/singleton/SingletonLazy.java
@@ -1,22 +1,6 @@
-package one.digitalinnovation.gof.singleton;
-
-/**
- * Singleton "preguiçoso".
- * 
- * @author falvojr
- */
-public class SingletonLazy {
-
-	private static SingletonLazy instancia;
-	
-	private SingletonLazy() {
-		super();
+public static synchronized SingletonLazy getInstancia() {
+	if (instancia == null) {
+		instancia = new SingletonLazy();
 	}
-	
-	public static SingletonLazy getInstancia() {
-		if (instancia == null) {
-			instancia = new SingletonLazy();
-		}
-		return instancia;
-	}
+	return instancia;
 }
