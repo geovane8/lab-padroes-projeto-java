087992badefe1422a2ebe72243fefb9625fe90bc
Geovane Nascimento <67743864+geovane8@users.noreply.github.com>
2023-04-29 00:25:45 -0300

    Update SingletonEager.java

--- src/one/digitalinnovation/gof/singleton/SingletonEager.java
+++ src/one/digitalinnovation/gof/singleton/SingletonEager.java
@@ -2,22 +2,25 @@ package one.digitalinnovation.gof.singleton;
 
 /**
  * Singleton "apressado".
- * 
- * @author
  */
 public final class SingletonEager {
 
-	private static final SingletonEager INSTANCIA = new SingletonEager();
-	
-	private SingletonEager() {
-		// previne a instanciação direta da classe
-	}
-	
-	public static SingletonEager getInstancia() {
-		return INSTANCIA;
-	}
-	
-	public void metodoExemplo() {
-		// método de exemplo
-	}
+    private static final SingletonEager INSTANCIA = new SingletonEager();
+
+    private SingletonEager() {
+        // previne a instanciação direta da classe
+    }
+
+    /**
+     * Retorna a instância única deste singleton.
+     *
+     * @return a instância única deste singleton
+     */
+    public static SingletonEager getInstancia() {
+        return INSTANCIA;
+    }
+
+    public void metodoExemplo() {
+        // implementation details
+    }
 }
